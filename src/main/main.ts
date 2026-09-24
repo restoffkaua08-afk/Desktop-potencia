82effc52ea2c1771e3926ed856bc45a1024a38cd
Kauã <restoffkaua08@gmail.com>
2026-09-23 21:46:32 -0300

    chore: complete desktop application shell

---
+++ src/main/main.ts
@@ -0,0 +1,35 @@
+import { app, BrowserWindow } from "electron";
+import { join } from "node:path";
+
+function createWindow() {
+  const window = new BrowserWindow({
+    width: 1440,
+    height: 900,
+    minWidth: 1000,
+    minHeight: 650,
+    backgroundColor: "#050505",
+    webPreferences: {
+      preload: join(__dirname, "../preload/preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  if (process.env.ELECTRON_RENDERER_URL) {
+    void window.loadURL(process.env.ELECTRON_RENDERER_URL);
+  } else {
+    void window.loadFile(join(__dirname, "../renderer/index.html"));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
